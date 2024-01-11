7be723983d14404d90197978afd2dc2ca0d4bc45
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-01-10 21:29:22 -0300

    database and environment variables configurations

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
 import fastify from 'fastify'
+import crypto from 'crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/', async (request, reply) => {
-  return 'hello world'
+app.get('/hello', async () => {
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('server listening on 3333')
