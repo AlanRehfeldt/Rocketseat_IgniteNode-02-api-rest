aa10bc3de43157abcfc141f01fcbc6e1a9908d4a
Alan Rehfeldt <alanrehfeldt@gmail.com>
2024-06-16 22:14:06 -0300

    remove unused imports

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
-
-import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify()
 
 app.register(cookie)
 
-app.addHook('preHandler', async (request, reply) => {
+app.addHook('preHandler', async (request) => {
   console.log(`[${request.method}] ${request.url}`)
 })
 
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { FastifyInstance } from 'fastify'
 import { string, z } from 'zod'
 import { randomUUID } from 'crypto'
